299de3b9834d3cd1a22c885d05cf22c436632596
Julio Corradini <julcorradi@gmail.com>
2023-11-14 18:33:16 -0300

    Refactorizando el código.

--- Analizador_de_sentimientos.py
+++ Analizador_de_sentimientos.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify
 from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 
+# Descargar el recurso vader_lexicon
+nltk.download('vader_lexicon')
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -16,21 +19,13 @@ def index():
 @app.route("/analisis", methods=["POST"])
 @cross_origin()
 def reques_front_end(): 
-  ##/response = requests.get ("http://localhost:8080/respuesta")
 
-  response = requests.get_json()
-  ##/response_string = ""
+  data = request.get_json()
+  
   texto_recibido = data['texto']
-
-  ##/if response.status_code == 200:
-  ##/  response_string = response.text
-  ##/  print (response_string)
-  ##/else:
-  ##/  print('La solicitud no se pudo realizar correctamente')
 
   sia = SentimentIntensityAnalyzer()
 
-  ##/sentimiento = sia.polarity_scores(response_string)['compound']
   sentimiento = sia.polarity_scores(texto_recibido)['compound']
 
   def valoracion (sentimiento):
@@ -42,18 +37,12 @@ def reques_front_end():
       valoracion = 'Valoracion Negativa'
     else:
       valoracion = 'No se pudo determinar la intenicon del texto'
-      
-    ##/data = {"txtoAnalisis": valoracion}
-    return {'valoracion': valoracion}
-      
-    ##/valoration = requests.post ("http://localhost:8080/analisisPOST", json=data)
-      
-    ##/if valoration.status_code == 200:
-    ##/  print("POST exitoso")
-    ##/else:
-    ##/  print("Error en el POST: ", valoration.status_code)
 
-  ##/valoracion(sentimiento)
+    return {'valoracion': valoracion}
+  
+  result = valoracion (sentimiento)
+
+  return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
